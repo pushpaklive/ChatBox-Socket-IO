0158b99ecaa9a0c24f831d008af1957243379ab1
Pushpak Choudhary <pushpaklive@gmail.com>
2018-11-04 20:01:59 +0530

    added login using mongoose

--- app.js
+++ app.js
@@ -3,13 +3,72 @@ var path = require('path');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+var mongooose = require('mongoose');
 
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var db = mongooose.connect("mongodb://username:passsword@ds239071.mlab.com:39071/dbname",
+    function (err, response) {
+        if (err)
+            console.log("Error in connecting to pushpak-db : error is : " + err);
+        console.log("Successfully connected to " + db + " : response : " + response);
+    });
 
 app.use('/static', express.static('assets'));
 app.use('/styles', express.static('css'));
 
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:2000");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,PATCH,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+
+
 app.get('/', function (req, res) {
-    res.sendfile(__dirname+'/chat.html');
+    res.sendfile(__dirname + '/login.html');
+})
+
+var Schema = mongooose.Schema;
+
+var UsersSchema = new Schema({
+    user: { type: String },
+    pwd: { type: String }
+}, { versionKey: false });
+
+var model = mongooose.model("user", UsersSchema, "user");
+
+app.post("/login", function(req, res){
+    var currModel = new model( req.body );
+    currModel.save(function (err, data) {
+        if (err)
+            console.log("error in api /login : error : ", err);
+            console.log("User added successfully!!!!!!")
+            res.sendfile(__dirname+'/chat.html');
+    })
+    
+})
+
+app.post("/canstartchat", function(req, res){
+    var values = new model(req.body);
+    var user = req.body.user;
+    var pwd = req.body.pwd;
+    console.log(user,pwd,"skwnhkjdhsnkjhs")
+    model.findOne({user:user, pwd:pwd}, function(err, user){
+        if(err)
+        console.log("err : /startchat --> "+err);
+        if(user == null || user == undefined){
+            console.log("err : /startchat --> "+err);
+            res.sendfile(__dirname+'/error.html');
+        }
+       else{
+        console.log("user : "+user);
+        res.sendfile(__dirname+'/chat.html');
+       }       
+    })
 })
 
 /* io.on("connection", function (socket) {
@@ -62,23 +121,23 @@ io.on('connection', function(socket) {
 }) */
 
 var users = [];
-io.on('connection', function(socket) {
-   console.log('A user connected');
-   socket.on('setUsername', function(value) {
-      console.log("value : "+value);
-      console.log("users.indexof(data) : "+users.indexOf(value));
-      if(users.indexOf(value) > -1) {
-         socket.emit('userExists', value + ' username is taken! Try some other username.');
-      } else {
-         users.push(value);
-         socket.emit('userSet', {username: value});
-      }
-   });
-   
-   socket.on('msg', function(data) {
-      //Send message to everyone
-      io.sockets.emit('newmsg', data);
-   })
+io.on('connection', function (socket) {
+    console.log('A user connected');
+    socket.on('setUsername', function (value) {
+        console.log("value : " + value);
+        console.log("users.indexof(data) : " + users.indexOf(value));
+        if (users.indexOf(value) > -1) {
+            socket.emit('userExists', value + ' username is taken! Try some other username.');
+        } else {
+            users.push(value);
+            socket.emit('userSet', { username: value });
+        }
+    });
+
+    socket.on('msg', function (data) {
+        //Send message to everyone
+        io.sockets.emit('newmsg', data);
+    })
 });
 
 http.listen(2000, function () {
